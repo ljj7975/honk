c888fc2be7b6521363a774ff898cc22557a18ebd
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-02-23 16:39:04 -0500

    graphs updated

--- graphs/data_size.py
+++ graphs/data_size.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=[7,7])
+fig, axs = plt.subplots(2, 2, figsize=[7,7])
+
+fig.suptitle('Size of audio per keyword\n( learning rate = 0.01, n_epochs = 20 )')
+fig.subplots_adjust(hspace=0.35)
+
 datasize = np.arange(0, 11)
 
 # brandon 
@@ -11,39 +15,56 @@ datasize = np.arange(0, 11)
 original = [0.9204, 0.9219, 0.9208, 0.9202, 0.923, 0.9235, 0.8761, 0.893, 0.9076, 0.9083, 0.8818]
 personalized = [0.7083, 0.7083, 0.7083, 0.7083, 0.7083, 0.7083, 0.9583, 0.9417, 0.925, 0.8917, 0.9667]
 
-ax1.plot(datasize, original, '--', label='original')
-ax1.plot(datasize, personalized, '-', label='personalized')
-ax1.set(xlabel='Size per keyword', ylabel='Accuracy', xticks=datasize)
-ax1.set_ylim(0.7, 1.0)
-ax1.grid()
-
-# Shrink current axis's height by 10% on the bottom
-box = ax1.get_position()
-ax1.set_position([box.x0, box.y0 + box.height * 0.2,
-                  box.width, box.height * 0.8])
-
-ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
-           fancybox=True, shadow=True, ncol=4, prop={'size': 10})
-
+axs[0][0].set_title('Brandon')
+original_line = axs[0][0].plot(datasize, original, '--', label='original')
+personalized_line = axs[0][0].plot(datasize, personalized, '-', label='personalized')
+axs[0][0].set(xlabel='Size per keyword', ylabel='Accuracy', xticks=datasize)
+axs[0][0].set_ylim(0.7, 1.0)
+axs[0][0].grid()
 
 # jay
 
 original = [0.9204, 0.9219, 0.9208, 0.9202, 0.923, 0.9235, 0.8943, 0.9018, 0.8994, 0.888, 0.9008]
 personalized = [0.875, 0.875, 0.875, 0.875, 0.875, 0.875, 0.9417, 0.925, 0.95, 0.9167, 0.9333]
 
-ax2.plot(datasize, original, '--', label='original')
-ax2.plot(datasize, personalized, '-', label='personalized')
-ax2.set(xlabel='Size per keyword', ylabel='Accuracy', xticks=datasize)
-ax2.set_ylim(0.7, 1.0)
-ax2.grid()
+axs[0][1].set_title('Jay')
+original_line = axs[0][1].plot(datasize, original, '--', label='original')
+personalized_line = axs[0][1].plot(datasize, personalized, '-', label='personalized')
+axs[0][1].set(xlabel='Size per keyword', ylabel='Accuracy', xticks=datasize)
+axs[0][1].set_ylim(0.7, 1.0)
+axs[0][1].grid()
 
-# Shrink current axis's height by 10% on the bottom
-box = ax2.get_position()
-ax2.set_position([box.x0, box.y0 + box.height * 0.2,
-                  box.width, box.height * 0.8])
 
-ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
-           fancybox=True, shadow=True, ncol=4, prop={'size': 10})
+# jack
 
-# fig.savefig("data_size.png")
-plt.show()
+original = [0.9204, 0.9219, 0.9208, 0.9202, 0.923, 0.9235, 0.9157, 0.8992, 0.9018, 0.895, 0.8994]
+personalized = [0.7667, 0.7667, 0.7667, 0.7667, 0.7667, 0.7667, 0.8417, 0.8667, 0.8583, 0.85, 0.7917]
+
+axs[1][0].set_title('Jack')
+original_line = axs[1][0].plot(datasize, original, '--', label='original')
+personalized_line = axs[1][0].plot(datasize, personalized, '-', label='personalized')
+axs[1][0].set(xlabel='Size per keyword', ylabel='Accuracy', xticks=datasize)
+axs[1][0].set_ylim(0.7, 1.0)
+axs[1][0].grid()
+
+
+# max
+
+original = [0.9204, 0.9219, 0.9208, 0.9202, 0.923, 0.9235, 0.9056, 0.9019, 0.8921, 0.8997, 0.9018]
+personalized = [0.7833, 0.7833, 0.7833, 0.7833, 0.7833, 0.7833, 0.9333, 0.9417, 0.925, 0.9417, 0.95]
+
+axs[1][1].set_title('Max')
+original_line = axs[1][1].plot(datasize, original, '--', label='original')
+personalized_line = axs[1][1].plot(datasize, personalized, '-', label='personalized')
+axs[1][1].set(xlabel='Size per keyword', ylabel='Accuracy', xticks=datasize)
+axs[1][1].set_ylim(0.7, 1.0)
+axs[1][1].grid()
+
+
+fig.legend([original_line, personalized_line],     # The line objects
+           labels=["original", "personalized"],   # The labels for each line
+           loc=8,  # Position of legend
+           ncol=2
+           )
+fig.savefig("data_size.png")
+# plt.show()
--- graphs/learning_rate.py
+++ graphs/learning_rate.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=[7,7])
+fig, axs = plt.subplots(2, 2, figsize=[7,7])
+
+fig.suptitle('Learning rate\n( size_per_word : 6, n_epochs = 20 )')
+fig.subplots_adjust(hspace=0.35)
+
 x_ticks = np.arange(1,5)
 lr = [0.1, 0.01, 0.001, 0.0001]
 
@@ -12,19 +16,12 @@ lr = [0.1, 0.01, 0.001, 0.0001]
 original = [0.6906, 0.8644, 0.9069, 0.9061]
 personalized = [0.925, 0.9667, 0.8833, 0.85]
 
-ax1.plot(x_ticks, original, '--', label='original')
-ax1.plot(x_ticks, personalized, '-', label='personalized')
-ax1.set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
-ax1.set_ylim(0.7, 1.0)
-ax1.grid()
-
-# Shrink current axis's height by 10% on the bottom
-box = ax1.get_position()
-ax1.set_position([box.x0, box.y0 + box.height * 0.2,
-                  box.width, box.height * 0.8])
-
-ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
-           fancybox=True, shadow=True, ncol=4, prop={'size': 10})
+axs[0][0].set_title('Brandon')
+original_line = axs[0][0].plot(x_ticks, original, '--', label='original')
+personalized_line = axs[0][0].plot(x_ticks, personalized, '-', label='personalized')
+axs[0][0].set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
+axs[0][0].set_ylim(0.7, 1.0)
+axs[0][0].grid()
 
 
 # jay
@@ -32,19 +29,42 @@ ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
 original = [0.8608, 0.8996, 0.9107, 0.9124]
 personalized = [0.925, 0.9583, 0.925, 0.9]
 
-ax2.plot(x_ticks, original, '--', label='original')
-ax2.plot(x_ticks, personalized, '-', label='personalized')
-ax2.set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
-ax2.set_ylim(0.7, 1.0)
-ax2.grid()
+axs[0][1].set_title('Jay')
+original_line = axs[0][1].plot(x_ticks, original, '--', label='original')
+personalized_line = axs[0][1].plot(x_ticks, personalized, '-', label='personalized')
+axs[0][1].set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
+axs[0][1].set_ylim(0.7, 1.0)
+axs[0][1].grid()
 
-# Shrink current axis's height by 10% on the bottom
-box = ax2.get_position()
-ax2.set_position([box.x0, box.y0 + box.height * 0.2,
-                  box.width, box.height * 0.8])
+# jack 
 
-ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.22),
-           fancybox=True, shadow=True, ncol=4, prop={'size': 10})
+original = [0.7742, 0.9032, 0.9023, 0.9107]
+personalized = [0.85, 0.8583, 0.8083, 0.7833]
 
-# fig.savefig("learning_rate.png")
-plt.show()
+axs[1][0].set_title('Jack')
+original_line = axs[1][0].plot(x_ticks, original, '--', label='original')
+personalized_line = axs[1][0].plot(x_ticks, personalized, '-', label='personalized')
+axs[1][0].set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
+axs[1][0].set_ylim(0.7, 1.0)
+axs[1][0].grid()
+
+# max
+
+original = [0.7753, 0.9118, 0.9083, 0.9116]
+personalized = [0.95, 0.9333, 0.9167, 0.875]
+
+axs[1][1].set_title('Max')
+original_line = axs[1][1].plot(x_ticks, original, '--', label='original')
+personalized_line = axs[1][1].plot(x_ticks, personalized, '-', label='personalized')
+axs[1][1].set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
+axs[1][1].set_ylim(0.7, 1.0)
+axs[1][1].grid()
+
+
+fig.legend([original_line, personalized_line],     # The line objects
+           labels=["original", "personalized"],   # The labels for each line
+           loc=8,  # Position of legend
+           ncol=2
+           )
+fig.savefig("learning_rate.png")
+# plt.show()
