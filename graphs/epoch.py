a04e027da85245e0d4114b0b630f0fca8b0a06f3
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-03-28 18:30:15 -0400

    updated graphs

--- graphs/epoch.py
+++ graphs/epoch.py
@@ -2,6 +2,7 @@ import os
 import ast
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 
 fig, axs = plt.subplots(2, 2, figsize=[7,7])
 
@@ -48,9 +49,6 @@ for exp in os.listdir(result_dir):
             per_acc[person] = summary['setting']['personlized_acc']
             results = summary[metric]
 
-            original[person]
-            personalized[person]
-
             for key in results.keys():
                 epochs = results[key]['epochs']
                 original[person][key].append(results[key]['original'])
@@ -58,10 +56,51 @@ for exp in os.listdir(result_dir):
 
 line_color = ['#fd0000', '#D6B748', '#45A320', '#5ADABE', '#4867D6']
 
-def get_average(arr):
-    return np.mean(np.array(arr), axis=0)
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
+
+def process_data(arr):
+
+    processed = []
+    for _ in epochs:
+        processed.append([])
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            processed[j].append(arr[i][j])
+
+    mean_arr = []
+    lower_arr = []
+    upper_arr = []
+
+    for data in processed:
+        mean, lower, upper = mean_confidence_interval(data)
+        mean_arr.append(mean)
+        lower_arr.append(lower)
+        upper_arr.append(upper)
+
+    return np.array(mean_arr), np.array(lower_arr), np.array(upper_arr)
+
+
+def plot_mean_and_CI(axis, mean, lb, ub, fmt=None, color_mean=None, color_shading=None, label=None):
+    # plot the shaded range of the confidence intervals
+    axis.fill_between(epochs, ub, lb, color=color_shading, alpha=.2)
+
+    # plot the mean on top
+    return axis.plot(epochs, mean, fmt, color=color_mean, label=label)
+
 
 legends = []
+name_mapping = {
+    "BRANDON":"A",
+    "JAY":"B",
+    "JACK":"C",
+    "MAX":"D"
+}
 
 for person, axis in axs_mapping.items():
     processed_original = []
@@ -70,14 +109,23 @@ for person, axis in axs_mapping.items():
     original_dict = original[person]
     personalized_dict = personalized[person]
 
-    axis.set_title('{0} - {1} %'.format(person.upper(), round(per_acc[person] * 100, 2)))
+    axis.set_title('{0} - {1} %'.format(name_mapping[person.upper()], round(per_acc[person] * 100, 2)))
     axis.set(xlabel='number of epoch', ylabel='accuracy', xticks=epochs[::2])
     axis.set_ylim(0.75, 1.0)
     axis.grid()
 
     for i in original[person].keys():
-        original_line = axis.plot(epochs, get_average(original_dict[i]), '--', color=line_color[int(i)-1], label='original')
-        personalized_line = axis.plot(epochs, get_average(personalized_dict[i]), '-', color=line_color[int(i)-1], label='personalized')
+        original_mean, original_lower, original_upper = process_data(original_dict[i])
+        original_error = [original_lower, original_upper]
+        personalized_mean, personalized_lower, personalized_upper = process_data(personalized_dict[i])
+        personalized_error = [personalized_lower, personalized_upper]
+
+        original_line = plot_mean_and_CI(axis, original_mean, original_lower, original_upper, fmt='--', color_mean=line_color[int(i)-1], color_shading=line_color[int(i)-1], label='original')
+
+        personalized_line = plot_mean_and_CI(axis, personalized_mean, personalized_lower, personalized_upper, fmt='-', color_mean=line_color[int(i)-1], color_shading=line_color[int(i)-1], label='personalized')
+
+        # original_line = axis.plot(epochs, get_average(original_dict[i]), '--', color=line_color[int(i)-1], label='original')
+        # personalized_line = axis.plot(epochs, get_average(personalized_dict[i]), '-', color=line_color[int(i)-1], label='personalized')
 
         legends.append(original_line)
         legends.append(personalized_line)
@@ -89,7 +137,7 @@ fig.legend(legends,     # The line objects
            ncol=3
            )
 
-fig.subplots_adjust(bottom=0.15, wspace=0.38, hspace=0.38, right=0.95)
+fig.subplots_adjust(bottom=0.15, wspace=0.38, hspace=0.38, right=0.96, left=0.11)
 fig.suptitle('Epoch\nbase model accuracy - {0} %'.format(round(base_model_acc * 100, 2)))
 
 fig.savefig("epoch.png")
--- graphs/learning_rate.py
+++ graphs/learning_rate.py
@@ -2,6 +2,7 @@ import os
 import ast
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 
 fig, axs = plt.subplots(2, 2, figsize=[7,7])
 
@@ -51,9 +52,6 @@ for exp in os.listdir(result_dir):
             per_acc[person] = summary['setting']['personlized_acc']
             results = summary[metric]
 
-            original[person]
-            personalized[person]
-
             for key in results.keys():
                 lr = results[key]['lr']
                 original[person][key].append(results[key]['original'])
@@ -61,11 +59,48 @@ for exp in os.listdir(result_dir):
 
 line_color = ['#fd0000', '#D6B748', '#45A320', '#5ADABE', '#4867D6']
 
-def get_average(arr):
-    return np.mean(np.array(arr), axis=0)
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
+
+def process_data(arr):
+    processed = [[],[],[],[]]
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            processed[j].append(arr[i][j])
+
+    mean_arr = []
+    lower_arr = []
+    upper_arr = []
+
+    for data in processed:
+        mean, lower, upper = mean_confidence_interval(data)
+        mean_arr.append(mean)
+        lower_arr.append(lower)
+        upper_arr.append(upper)
+
+    return np.array(mean_arr), np.array(lower_arr), np.array(upper_arr)
 
 legends = []
 x_ticks = np.arange(1,5)
+
+name_mapping = {
+    "BRANDON":"A",
+    "JAY":"B",
+    "JACK":"C",
+    "MAX":"D"
+}
+
+def plot_mean_and_CI(axis, mean, lb, ub, fmt=None, color_mean=None, color_shading=None, label=None):
+    # plot the shaded range of the confidence intervals
+    axis.fill_between(x_ticks, ub, lb, color=color_shading, alpha=.2)
+
+    # plot the mean on top
+    return axis.plot(x_ticks, mean, fmt, color=color_mean, label=label)
 
 for person, axis in axs_mapping.items():
     processed_original = []
@@ -74,19 +109,27 @@ for person, axis in axs_mapping.items():
     original_dict = original[person]
     personalized_dict = personalized[person]
 
-    axis.set_title('{0} (per : {1})'.format(person, round(per_acc[person], 4)))
+    axis.set_title('{0} - {1} %'.format(name_mapping[person.upper()], round(per_acc[person] * 100, 2)))
 
-    axis.set(xlabel='Learning rate', ylabel='Accuracy', xticks=x_ticks, xticklabels=lr)
+    axis.set(xlabel='learning rate', ylabel='accuracy', xticks=x_ticks, xticklabels=lr)
     axis.set_ylim(0.7, 1.0)
     axis.grid()
 
     for i in original[person].keys():
-        original_line = axis.plot(x_ticks, get_average(original_dict[i]), '--', color=line_color[int(i)-1], label='original')
-        personalized_line = axis.plot(x_ticks, get_average(personalized_dict[i]), '-', color=line_color[int(i)-1], label='personalized')
+        original_mean, original_lower, original_upper = process_data(original_dict[i])
+        original_error = [original_lower, original_upper]
+        personalized_mean, personalized_lower, personalized_upper = process_data(personalized_dict[i])
+        personalized_error = [personalized_lower, personalized_upper]
+
+        original_line = plot_mean_and_CI(axis, original_mean, original_lower, original_upper, fmt='--', color_mean=line_color[int(i)-1], color_shading=line_color[int(i)-1], label='original')
+
+        personalized_line = plot_mean_and_CI(axis, personalized_mean, personalized_lower, personalized_upper, fmt='-', color_mean=line_color[int(i)-1], color_shading=line_color[int(i)-1], label='personalized')
+
+#         original_line = axis.plot(x_ticks, original_mean, '--', color=line_color[int(i)-1], label='original')
+#         personalized_line = axis.plot(x_ticks, personalized_mean, '-', color=line_color[int(i)-1], label='personalized')
 
         legends.append(original_line)
         legends.append(personalized_line)
-
 
 fig.legend(legends,     # The line objects
            labels=["original_1", "personalized_1", "original_3", "personalized_3", "original_5", "personalized_5"],   # The labels for each line
@@ -94,8 +137,10 @@ fig.legend(legends,     # The line objects
            ncol=3
            )
 
-fig.subplots_adjust(bottom=0.15, wspace=0.28, hspace=0.35)
-fig.suptitle('Learning Rate ( epochs = {0} )\nbase model acc : {1}'.format(epochs, round(base_model_acc, 4)))
+
+fig.subplots_adjust(bottom=0.15, wspace=0.38, hspace=0.38, right=0.96, left=0.11)
+# fig.suptitle('Learning Rate ( epochs = {0} )\nbase model accuracy : {1} %'.format(epochs, round(base_model_acc * 100, 2)))
+fig.suptitle('Learning Rate\nbase model accuracy - {0} %'.format(round(base_model_acc * 100, 2)))
 
 fig.savefig("learning_rate.png")
 plt.show()
